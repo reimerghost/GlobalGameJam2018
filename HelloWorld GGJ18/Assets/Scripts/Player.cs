dba4546f18f0ca584c728cd679b68efd57a7e3f2
ReimerGhost <rchamale10@gmail.com>
2018-01-27 11:55:54 -0600

    Jugador en movimiento.

---
+++ HelloWorld GGJ18/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+
+
+
+    private float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+    private float sprintSpeed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        walkSpeed = (float)(5 + (14 / 5));
+        sprintSpeed = walkSpeed + (walkSpeed / 2);
+        
+    }
+
+    void FixedUpdate()
+    {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+
+        // Move senteces
+        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    }
+    
+}
