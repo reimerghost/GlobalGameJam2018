6f4ba5de134aeb57e37fc1d88399a1471e34022c
ReimerGhost <rchamale10@gmail.com>
2018-01-27 10:43:26 -0600

    RumbleTest.

---
+++ HelloWorld GGJ18/Assets/Script/RumbleCon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumbleCon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int horizontal = 0;
+        int vertical = 0;
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        Debug.Log("H: " + horizontal);
+        Debug.Log("V: " + vertical);
+
+        Handheld.Vibrate();
+
+        if (horizontal != 0)
+            vertical = 0;
+
+
+        
+    }
+}
