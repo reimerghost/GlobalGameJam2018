61a72534066ffc888e56fc7d2777be13f06c6dd0
DevAvalos <201114339@ingenieria.usac.edu.gt>
2018-01-28 15:33:30 -0600

    AgregarPalanca

--- HelloWorld GGJ18/Assets/Scripts/MapDefinition.cs
+++ HelloWorld GGJ18/Assets/Scripts/MapDefinition.cs
@@ -3,13 +3,14 @@
 public class MapDefinition
 {
 
-    public static readonly String[] Player1Mapping = new String[] { "Floor1;Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1;" +
-               "Muro1,Palanca,#,#,BloqueoA,#,#,Muro1;"+
+    public static readonly String[] Player1Mapping = new String[] {
+        "Floor1;Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1;" +
+               "Muro1,Palanca,##,#,BloqueoA,#,#,Muro1;"+
                "Muro1,#####,########,Muro1,Muro1,Muro1,Muro1,Muro1;"+
                "Muro1,Muro1,BloqueoA,Muro1,#####,BloqueoA,#####,Muro1;"+
                "Muro1,Muro1,########,Muro1,#####,BloqueoA,#####,Muro1;"+
                "Muro1,Muro1,########,Muro1,#####,Muro1,########,Muro1;"+
-               "Muro1,Muro1,BloqueoA,#####,BloqueoB,Muro1,Exit2,Muro1;"+
+               "Muro1,Muro1,BloqueoA,Palanca,BloqueoB,Muro1,Exit2,Muro1;"+
                "Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1"
         , "Floor2;Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1;" +
                "Muro1,Palanca,#,#,BloqueoA,#,#,Muro1;"+
@@ -35,7 +36,7 @@ public class MapDefinition
                "Muro1,Muro1,BloqueoA,Muro1,#####,BloqueoA,#####,Muro1;"+
                "Muro1,Muro1,########,Muro1,#####,BloqueoA,#####,Muro1;"+
                "Muro1,Muro1,########,Muro1,#####,Muro1,########,Muro1;"+
-               "Muro1,Muro1,BloqueoA,#####,BloqueoB,Muro1,Exit1,Muro1;"+
+               "Muro1,Muro1,BloqueoA,Palanca,BloqueoB,Muro1,Exit1,Muro1;"+
                "Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1"
         ,  "Floor5;Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1,Muro1;" +
                "Muro1,Palanca,#,#,BloqueoA,#,#,Muro1;"+
